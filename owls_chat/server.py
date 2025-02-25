1dcad6b8dfb0fbd2d1fd8e9f6b6a69091a1c71f0
Brett Jurman <i.be.brett@gmail.com>
2025-02-25 18:53:27 -0500

    hacks

--- owls_chat/server.py
+++ owls_chat/server.py
@@ -5,6 +5,7 @@ MAX_NAME_LENGTH = 100
 MAX_MESSAGE_LENGTH = 1000
 MAX_LINE_LENGTH = 2048
 
+
 class Client:
     def __init__(self, name, writer, reader):
         self.name = name
@@ -13,30 +14,27 @@ class Client:
         self.last_message = None
         self.out_messages = []
 
+
 class Server:
     def __init__(self):
         self.clients = {}
-        
+
     async def handle_client(self, client):
         pass
 
     async def start(self, host_address, host_port):
-        server = await asyncio.start_server(
-            self.handle_client,
-            host_address,
-            host_port
-        )
+        server = await asyncio.start_server(self.handle_client, host_address, host_port)
 
         async with server:
             await server.serve_forever()
 
     async def get_line(self, reader) -> str:
-        res = (await reader.readline()).decode()
-        return res
+        b = (await reader.readline())
+        return b.decode('utf-8',codec_errors='replace')
+        
 
     async def write_line(self, writer, line: str):
-        return writer.write(f'{line}\n'.encode())
-
+        return writer.write(f"{line}\n".encode())
 
     async def broadcast(self, message: str):
         for c in self.clients.values():
@@ -51,7 +49,7 @@ class Server:
             if len(name) > 100 or not name or name.startswith("/"):
                 await self.write_line(writer, "bad name")
                 return  # break if we don't like the name
-        
+
             if name in self.clients:
                 await self.write_line(writer, "Someone with that name is already here")
                 return
@@ -62,9 +60,9 @@ class Server:
 
             await self.write_line(writer, f"Welome {name}")
             client = Client(
-                    name=name,
-                    writer=writer,
-                    reader=reader,
+                name=name,
+                writer=writer,
+                reader=reader,
             )
 
             self.clients[name] = client
@@ -76,7 +74,7 @@ class Server:
                 # with slash
                 if msg == "/rollcall":
                     all_clients = ", ".join(self.clients.keys())
-                    await self.broadcast(f'/rollcall: {all_clients}')
+                    await self.broadcast(f"/rollcall: {all_clients}")
                 else:
                     await self.broadcast(f'{name}: "{msg}"')
 
@@ -89,7 +87,8 @@ class Server:
 
 async def main_async():
     server = Server()
-    await server.start('0.0.0.0', 8080)
+    await server.start("0.0.0.0", 8080)
+
 
 def main():
     asyncio.run(main_async())
